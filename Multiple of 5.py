2c41a5c81901cdfffc10847939eb762325ebe842
Arjun M <90825849+Arjun8420@users.noreply.github.com>
2021-10-05 08:40:11 +0530

    Add files via upload

---
+++ Multiple of 5.py
@@ -0,0 +1,8 @@
+#Program to verify the number is a multiple of 5
+
+num=int(input("Enter the number = "))
+
+if num%5==0:
+    print(num,"is a multiple of 5")
+else:
+    print(num,"is not a multiple of 5")
